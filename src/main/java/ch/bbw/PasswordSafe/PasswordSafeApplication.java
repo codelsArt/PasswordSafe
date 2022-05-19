094b66b4ebb0c61f2e34e84358961e05eba6cc0e
unknown <lucas.mancini@espas.ch>
2022-05-19 23:19:59 +0200

    initial commit

---
+++ src/main/java/ch/bbw/PasswordSafe/PasswordSafeApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.PasswordSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/PasswordSafe/PasswordSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.PasswordSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
